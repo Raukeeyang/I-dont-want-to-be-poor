91aebb34d16cb2e64ca788e4fb63795f23abc7ab
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2021-03-04 18:26:06 +0100

    Sistema de inventario

---
+++ I don't want to be poor/Assets/Scrpits/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int frutasMax;
+    public int frutasActuales;
+    public int platanos;
+    public int uvas;
+    public int durians;
+    public int sandias;
+
+    void RecibirFruta(int fruta)
+    {
+        if (frutasMax > frutasActuales)
+        {
+            switch (fruta)
+            {
+                case 1:
+                    platanos += 1;
+                    break;
+                case 2:
+                    uvas += 1;
+                    break;
+                case 3:
+                    sandias += 1;
+                    break;
+                case 4:
+                    durians += 1;
+                    break;
+
+            }
+        }
+    }
+
+    void UsarFruta(int fruta)
+    {
+        switch (fruta)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
